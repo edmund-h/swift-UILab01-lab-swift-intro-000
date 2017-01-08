e3c4718b0cf2d668100051adf2144dcd788098aa
edmund-h <447ght@gmail.com>
2017-01-08 14:57:02 -0500

    completed assignment

--- LightBulb/ViewController.swift
+++ LightBulb/ViewController.swift
@@ -11,20 +11,27 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var lightBulb: UIImageView!
-    
+    @IBOutlet weak var colorPicker: UISegmentedControl!
     
     override func viewDidLoad() {
         
         super.viewDidLoad()
         
-        lightBulb.backgroundColor = UIColor.blue
-        
+        lightBulb.backgroundColor = UIColor.black
+        colorPicker.setTitle("Red", forSegmentAt: 0)
+        colorPicker.setTitle("Yellow", forSegmentAt: 1)
+        colorPicker.setTitle("Blue", forSegmentAt: 2)
+        colorPicker.setTitle("Green", forSegmentAt: 3)
     }
     
     
     @IBAction func colorSelected(_ sender: UISegmentedControl) {
         
         print(sender.selectedSegmentIndex)
+        if colorPicker.selectedSegmentIndex == 0 { lightBulb.backgroundColor = UIColor.red}
+        if colorPicker.selectedSegmentIndex == 1 { lightBulb.backgroundColor = UIColor.yellow}
+        if colorPicker.selectedSegmentIndex == 2 { lightBulb.backgroundColor = UIColor.green}
+        if colorPicker.selectedSegmentIndex == 3 { lightBulb.backgroundColor = UIColor.blue}
         
     }
     
